f53c611d97c1b0872d7981a9128791145482746d
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-02-19 14:44:26 -0500

    Fix: corrected logic

--- app/main.py
+++ app/main.py
@@ -8,19 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result_list = []
-    for person in people:
-        name = person["name"]
-        age = person["age"]
-        result_list.append(Person(name, age))
+    result_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         name = person["name"]
         obj = Person.people[name]
-        if "wife" in person and person["wife"] is not None:
+        if "wife" in person and person.get("wife") is not None:
             wife_name = person["wife"]
             obj.wife = Person.people[wife_name]
-        elif "husband" in person and person["husband"] is not None:
+        elif "husband" in person and person.get("husband") is not None:
             husband_name = person["husband"]
             obj.husband = Person.people[husband_name]
 
