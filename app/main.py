e57a50232976879f09a3def35b733d2981779b67
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-02-19 15:02:17 -0500

    Use dict.get() instead of direct indexing

--- app/main.py
+++ app/main.py
@@ -8,16 +8,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result_list = [Person(person["name"], person["age"]) for person in people]
+    result_list = [Person(person.get("name"), person.get("age"))
+                   for person in people]
 
     for person in people:
-        name = person["name"]
+        name = person.get("name")
         obj = Person.people[name]
         if "wife" in person and person.get("wife") is not None:
-            wife_name = person["wife"]
+            wife_name = person.get("wife")
             obj.wife = Person.people[wife_name]
         elif "husband" in person and person.get("husband") is not None:
-            husband_name = person["husband"]
+            husband_name = person.get("husband")
             obj.husband = Person.people[husband_name]
 
     return result_list
